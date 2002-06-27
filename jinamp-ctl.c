767cb261adc07ecf158507bf1177530acebf3b34
Bruce Merry <bmerry@gmail.com>
2002-06-27 12:17:54 +0000

    stub

---
+++ jinamp-ctl.c
@@ -0,0 +1,34 @@
+/*
+ $Id: jinamp-ctl.c,v 1.1 2002/06/27 12:17:54 bruce Exp $
+
+ jinamp: a command line music shuffler
+ Copyright (C) 2001, 2002  Bruce Merry.
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License version 2 as
+ published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ Note: ONLY version 2 of the GPL is allowed. If the FSF release a later version I will
+ decide whether to relicense the software under it.
+ */
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <control.h>
+
+int main() {
+  return 0;
+}
