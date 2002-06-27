a4bfdfd8479684250d7ea354e5a9f89bbc31030b
Bruce Merry <bmerry@gmail.com>
2002-06-27 12:19:28 +0000

    Put back dev_null conditional on DEBUG

--- jinamp.c
+++ jinamp.c
@@ -1,5 +1,5 @@
 /*
- $Id: jinamp.c,v 1.7 2002/06/27 11:39:30 bruce Exp $
+ $Id: jinamp.c,v 1.8 2002/06/27 12:19:28 bruce Exp $
 
  jinamp: a command line music shuffler
  Copyright (C) 2001, 2002  Bruce Merry.
@@ -521,6 +521,9 @@ int options(int argc, char *argv[]) {
 
 int main(int argc, char *argv[]) {
   int first;
+#ifndef DEBUG
+  int dev_null;
+#endif
 
   /* if SUID is set, gain higher priority and drop root */
   if (geteuid() == 0) {
